0de138fa1ee6de9acda06d52321298d81303fe7d
Leandro Novais <90646451+lenovais56@users.noreply.github.com>
2022-08-24 16:16:18 -0300

    first commit: Desenvolvimento do cabeçalho com menu hamburguer, banner, seção do carrosel e seção Quem somos

---
+++ assets/js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 10,
+    slidesPerview: 1,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+  });
